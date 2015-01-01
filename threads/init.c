8fac0c79316f109ddc21cea28c165a4743ffb8e2
Amine Hajyoussef <hajyoussef.amine@gmail.com>
2015-01-01 13:32:12 +0100

    forget to add process initialization call upon kernel load

--- threads/init.c
+++ threads/init.c
@@ -134,7 +134,7 @@ main (void)
 
   /* Finish up. */
   shutdown ();
-  thread_exit ();
+  thread_exit (0);
 }
 
 /* Clear the "BSS", a segment that should be initialized to
@@ -285,6 +285,7 @@ run_task (char **argv)
   
   printf ("Executing '%s':\n", task);
 #ifdef USERPROG
+  process_init();
   process_wait (process_execute (task));
 #else
   run_test (task);
